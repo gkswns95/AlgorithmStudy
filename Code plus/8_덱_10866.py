08b491c10a7bd98531b1db06712a9bb535494753
gkswns95 <69138191+gkswns95@users.noreply.github.com>
2022-02-05 17:16:31 +0900

    Create 8_덱_10866.py

---
+++ Code plus/8_덱_10866.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+
+deque = deque()
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == 'push_front':
+        deque.appendleft(cmd[1])
+    
+    elif cmd[0] == 'push_back':
+        deque.append(cmd[1])
+
+    elif cmd[0] == 'pop_front':
+        print(deque.popleft() if deque else -1)
+    
+    elif cmd[0] == 'pop_back':
+        print(deque.pop() if deque else -1)
+    
+    elif cmd[0] == 'size':
+        print(len(deque))
+    
+    elif cmd[0] == 'empty':
+        print(0 if deque else 1)
+    
+    elif cmd[0] == 'front':
+        print(deque[0] if deque else -1)
+
+    elif cmd[0] == 'back':
+        print(deque[-1] if deque else -1)
